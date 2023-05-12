2b7fac601ce9c06b074b3036a935b6b0d854db0d
matsu <102255791+m47su@users.noreply.github.com>
2023-05-12 16:43:43 -0300

    Add files via upload

--- Trabalho/conn.php
+++ Trabalho/conn.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=viagem;port=3307','root','');
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=tucas;port=3307','root','');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->exec("SET CHARACTER SET utf8");
 
